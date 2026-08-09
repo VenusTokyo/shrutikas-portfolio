8c82d096d0f4d17584d0bc2aad9998c1736136cd
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2026-08-09 23:11:26 +0530

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { Gochi_Hand } from 'next/font/google';
 import '../styles/globals.css';
 import './index.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const gochiHand = Gochi_Hand({
   weight: ['400'],
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
       className={`${gochiHand.variable}`}
     >
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
