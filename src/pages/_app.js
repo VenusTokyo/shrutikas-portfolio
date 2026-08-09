7ed67d4161e783f4b8cf033e7b87ca73413560af
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2026-08-09 23:07:39 +0530

    removed unnessary fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,35 +1,6 @@
-import { Playfair_Display, Caveat, EB_Garamond, Kalam, Gochi_Hand } from 'next/font/google';
+import { Gochi_Hand } from 'next/font/google';
 import '../styles/globals.css';
 import './index.css';
-
-const playfair = Playfair_Display({
-  weight: ['700', '900'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-playfair',
-  display: 'swap',
-});
-
-const caveat = Caveat({
-  weight: ['500', '600', '700'],
-  subsets: ['latin'],
-  variable: '--font-caveat',
-  display: 'swap',
-});
-
-const garamond = EB_Garamond({
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-garamond',
-  display: 'swap',
-});
-
-const kalam = Kalam({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--font-kalam',
-  display: 'swap',
-});
 
 const gochiHand = Gochi_Hand({
   weight: ['400'],
@@ -41,7 +12,7 @@ const gochiHand = Gochi_Hand({
 export default function App({ Component, pageProps }) {
   return (
     <div
-      className={`${playfair.variable} ${caveat.variable} ${garamond.variable} ${kalam.variable} ${gochiHand.variable}`}
+      className={`${gochiHand.variable}`}
     >
       <Component {...pageProps} />
     </div>
